ab9ab8447cc3ec38b541cb33e5252f681255d8f5
Nicolas Lemoine <nico.lemoine@gmail.com>
2023-09-19 17:46:26 +0200

    feat!: Timber 2 compatibility
    - Fix Timber functions
    - Add/fix Coding Standards

--- autoload.php
+++ autoload.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 require_once 'lib/functions.php';
 
-if ( function_exists( 'add_filter' ) ) {
-	require_once 'lib/twig.php';
+if (function_exists('add_filter')) {
+    require_once 'lib/twig.php';
 }
 
-if ( function_exists( 'add_action' ) ) {
-	require_once 'lib/wp-head.php';
+if (function_exists('add_action')) {
+    require_once 'lib/wp-head.php';
 }
